4a163e2c8a1a67ea504f943b43ab6e18665ffb85
GalihFajar <galihfajar00@gmail.com>
2020-09-30 12:19:55 +0700

    fixing source code

--- src/inherent-ndn-simulation.cpp
+++ src/inherent-ndn-simulation.cpp
@@ -22,6 +22,7 @@
 #include "ns3/network-module.h"
 #include "ns3/ndnSIM-module.h"
 #include "../src/ndnSIM/helper/ndn-link-control-helper.hpp"
+#include <bits/stdc++.h>
 
 namespace ns3 {
 
@@ -31,7 +32,8 @@ main(int argc, char* argv[])
   CommandLine cmd;
   cmd.Parse(argc, argv);
   bool isIPSimulation = false; //change to true if IPSimulation is desired
-  
+  bool isPriorityFIFO = false; //change to true if PriorityFifo is desired
+
   AnnotatedTopologyReader topologyReader("", 26);
   topologyReader.SetFileName("src/ndnSIM/examples/inherent-topology.txt");
   topologyReader.Read();
@@ -39,9 +41,11 @@ main(int argc, char* argv[])
   /****************************************************************************/
   // Install NDN stack on all nodes
   int RouterCSSize = 1000;
+  string csPolicy= "nfd::cs::lru";
   if(isIPSimulation) RouterCSSize = 1;
+  if(isPriorityFIFO) csPolicy = "nfd::cs::priority_fifo";
   ndn::StackHelper ndnHelper;
-  ndnHelper.setPolicy("nfd::cs::lru");
+  ndnHelper.setPolicy(csPolicy);
   ndnHelper.setCsSize(RouterCSSize);
   ndnHelper.InstallAll();
   /****************************************************************************/
@@ -51,19 +55,15 @@ main(int argc, char* argv[])
   /****************************************************************************/
   // Getting containers for the consumer/producer
   Ptr<Node> consumer1 = Names::Find<Node>("Src1");
-  Ptr<Node> consumer2 = Names::Find<Node>("Src2");
-  Ptr<Node> consumer3 = Names::Find<Node>("Src3");
-  Ptr<Node> consumer4 = Names::Find<Node>("Src4");
-  Ptr<Node> consumer5 = Names::Find<Node>("Src5");
-  Ptr<Node> consumer6 = Names::Find<Node>("Rtr1");
-  Ptr<Node> consumer7 = Names::Find<Node>("Rtr2");
-  Ptr<Node> consumer8 = Names::Find<Node>("Rtr3");
-  Ptr<Node> consumer9 = Names::Find<Node>("Rtr4");
-  Ptr<Node> consumer10 = Names::Find<Node>("Rtr9");
-  Ptr<Node> consumer11 = Names::Find<Node>("Rtr11");
-  Ptr<Node> consumer12 = Names::Find<Node>("Rtr12");
-  Ptr<Node> consumer13 = Names::Find<Node>("Rtr17");
-  Ptr<Node> consumer14 = Names::Find<Node>("Rtr19");
+  Ptr<Node> consumer2 = Names::Find<Node>("Src3");
+  Ptr<Node> consumer3 = Names::Find<Node>("Src4");
+  Ptr<Node> consumer4 = Names::Find<Node>("Rtr1");
+  Ptr<Node> consumer5 = Names::Find<Node>("Rtr3");
+  Ptr<Node> consumer6 = Names::Find<Node>("Src2");
+  Ptr<Node> consumer7 = Names::Find<Node>("Rtr11");
+  Ptr<Node> consumer8 = Names::Find<Node>("Rtr13");
+  Ptr<Node> consumer9 = Names::Find<Node>("Rtr17");
+  Ptr<Node> consumer10 = Names::Find<Node>("Rtr19");
 
   Ptr<Node> producer1 = Names::Find<Node>("Dst1");
   Ptr<Node> producer2 = Names::Find<Node>("Dst2");
@@ -102,17 +102,6 @@ main(int argc, char* argv[])
   consumerHelper.SetPrefix("/source");
   consumerHelper.Install(consumer10);
   
-  consumerHelper.SetPrefix("/source");
-  consumerHelper.Install(consumer11);
-
-  consumerHelper.SetPrefix("/source");
-  consumerHelper.Install(consumer12);
-
-  consumerHelper.SetPrefix("/source");
-  consumerHelper.Install(consumer13);
-
-  consumerHelper.SetPrefix("/source");
-  consumerHelper.Install(consumer14);
 
   /****************************************************************************/
   ndn::AppHelper producerHelper("ns3::ndn::Producer");
@@ -147,7 +136,13 @@ main(int argc, char* argv[])
   L2RateTracer::InstallAll("inherent-drop-trace-ip.txt", Seconds(1.0));
   ndn::L3RateTracer::InstallAll("inherent-rate-trace-ip.txt", Seconds(1.0));
   ndn::AppDelayTracer::InstallAll("inherent-app-delays-trace-ip.txt");
-  }else{
+  }
+  else if(isPriorityFIFO){
+  L2RateTracer::InstallAll("inherent-drop-trace-fifo.txt", Seconds(1.0));
+  ndn::L3RateTracer::InstallAll("inherent-rate-trace-fifo.txt", Seconds(1.0));
+  ndn::AppDelayTracer::InstallAll("inherent-app-delays-trace-fifo.txt");
+  }
+  else{
   L2RateTracer::InstallAll("inherent-drop-trace.txt", Seconds(1.0));
   ndn::L3RateTracer::InstallAll("inherent-rate-trace.txt", Seconds(1.0));
   ndn::AppDelayTracer::InstallAll("inherent-app-delays-trace.txt");
